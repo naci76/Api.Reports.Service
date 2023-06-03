43787c13c392864f5c19934725d464045ec78196
Dev100 <Dev100@LAPTOP-VVGUU7M2>
2023-06-03 16:42:44 +0300

    Veritabanı için tablolar oluşturuldu.

---
+++ Api.Reports.Entitiy/Migrations/20230603134147_tablolarolustu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Reports.Entitiy.Migrations
+{
+    public partial class tablolarolustu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HotelCount = table.Column<int>(type: "int", nullable: false),
+                    PhoneCount = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    RequestDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
